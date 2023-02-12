5c04cbb27fa9c023e237ab21121131015febe526
HeytorToito1996 <toitocarvalho@gmail.com>
2023-02-11 22:26:18 -0300

    adição do módulo MySQL2

--- models/Conexao.js
+++ models/Conexao.js
@@ -1,6 +1,6 @@
 const Sequelize = require ('sequelize');
 const sequelize = new Sequelize('sistemaos1','root','b5d422bc762d2bd2b570e58d273ce62d',{
-    host:"localhost",
+    host:"mysql-wfkj:3306",
     dialect:"mysql"
 });
 
